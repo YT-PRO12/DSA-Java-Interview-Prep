abbaddff31444f238743ed7ee29c9c7b2be92de5
Yatharth Goyal <yatharthgoyal657@gmail.com>
2026-08-15 15:14:53 +0530

    Add files via upload

---
+++ 00-DSA-Roadmap/01-Arrays/06-Kadane-Pattern/ 04-Best-Time-to-Buy-and-Sell-Stock/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,18 @@
+public class BestTimeToBuyAndSellStock {
+    public static int maxProfit(int[] prices) {
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            minPrice = Math.min(minPrice, prices[i]);
+            maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+        }
+
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        System.out.println("Maximum Profit: " + maxProfit(prices));
+    }
+}
